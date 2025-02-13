1c3b6a46e5e9bab0f8e883a7283367229c7c9975
AlfonsoRod <fonssitorodriguezgutt@gmail.com>
2025-02-13 13:58:19 +0100

    Project folder creation

---
+++ Entrega1Grupo16/entrega1grupo16/src/main/java/es/ticketmaster/entrega1grupo16/Entrega1grupo16Application.java
@@ -0,0 +1,13 @@
+package es.ticketmaster.entrega1grupo16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Entrega1grupo16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Entrega1grupo16Application.class, args);
+	}
+
+}
---
+++ Entrega1Grupo16/entrega1grupo16/src/test/java/es/ticketmaster/entrega1grupo16/Entrega1grupo16ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ticketmaster.entrega1grupo16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Entrega1grupo16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
