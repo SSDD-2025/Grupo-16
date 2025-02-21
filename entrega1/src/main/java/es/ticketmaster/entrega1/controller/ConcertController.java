85362caccb9ca77f85db36619518a166ce35438c
davidrisqgom <diznerr@gmail.com>
2025-02-21 23:05:21 +0100

    Made login compulsory in order to buy tickets.

--- entrega1/src/main/java/es/ticketmaster/entrega1/controller/ConcertController.java
+++ entrega1/src/main/java/es/ticketmaster/entrega1/controller/ConcertController.java
@@ -7,23 +7,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import es.ticketmaster.entrega1.service.ConcertService;
+import es.ticketmaster.entrega1.service.UserService;
 
 
-/**
- * 
- */
 @Controller
 public class ConcertController {
 
     @Autowired
     ConcertService concertService;
 
+    @Autowired
+    UserService userService;
+
+    /**
+     * When the user is logged, the ticket selection page of the concert defined by the id parameter is displayed.
+     * In other case, an error page is shown. It is compulsory for the user to be logged in order to buy tickets.
+     * @param model model of the dinamic HTML document
+     * @param id concert identification to load its ticket choosing page
+     * @return name of the html to be responded to the server petition
+     */
     @GetMapping("/concert/{id}")
     public String postMethodName(Model model, @PathVariable Long id) {
         
-        model.addAttribute("concert", concertService.getConcertById(id));
+        boolean userLogged = userService.isLogged();
 
-        return "ticket-selling";
+        if(userLogged){
+            model.addAttribute("concert", concertService.getConcertById(id));
+            return "ticket-selling";
+        } else {
+            return "error"; /*TO BE IMPLEMENTED -> Error page when non-logged users try to access ticket selection */
+        }
     }
     
     
