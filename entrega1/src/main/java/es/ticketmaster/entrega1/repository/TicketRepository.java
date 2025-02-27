afae2f872d3f723196328732de9148975db88888
AlfonsoRod <fonssitorodriguezgutt@gmail.com>
2025-02-27 11:56:37 +0100

    Fixed the creation of Tickets.

--- entrega1/src/main/java/es/ticketmaster/entrega1/repository/TicketRepository.java
+++ entrega1/src/main/java/es/ticketmaster/entrega1/repository/TicketRepository.java
@@ -4,10 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import es.ticketmaster.entrega1.model.Ticket;
-import es.ticketmaster.entrega1.model.UserEntity;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     public Ticket findTicketById(long id);
-    public Ticket findTicketByZoneAndTicketUser(String zone, UserEntity user);
 }
--- entrega1/src/main/java/es/ticketmaster/entrega1/service/TicketService.java
+++ entrega1/src/main/java/es/ticketmaster/entrega1/service/TicketService.java
@@ -15,13 +15,13 @@ import es.ticketmaster.entrega1.repository.UserRepository;
 @Service
 public class TicketService {
     @Autowired
-    public TicketRepository ticketRepository;
+    private TicketRepository ticketRepository;
     @Autowired
-    public UserRepository userRepository;
+    private UserRepository userRepository;
     @Autowired
-    public ConcertRepository concertRepository;
+    private ConcertRepository concertRepository;
     @Autowired
-    public ActiveUser activeUser;
+    private ActiveUser activeUser;
 
     /**
      * Obtain the ticket form the database.
@@ -33,33 +33,33 @@ public class TicketService {
     }
 
     /**
-     * This is a private method that creates a new ticket. It is used when the user has purchased a ticket, 
-     * in order to associate a ticket to a user.
+     * This is a private method that creates a new ticket and save it in the database.
      * @param zone of the ticket that has been purchased.
      * @param price of the ticket.
      * @param concert that corresponds to a certain ticket
      * @return the created ticket.
      */
     private Ticket createTicket(String zone, float price, Concert concert) {
-        return new Ticket(zone, price, null, concert);
+        Ticket newTicket = new Ticket(zone, price, null, concert);
+        this.ticketRepository.save(newTicket);
+        return newTicket;
     }
 
     /**
-     * This method will obtain the tickets the user has purchased. Once obtained will do the following things:
+     * This method will create a ticket (by calling the createTicket method). Once created will do the following things:
      * Adding that new ticket to the user ticket list.
      * Once the ticket has being associated to a user, it will be saved in the ticket`s repository.
      * Finally, the same thing will be happening with the user,
      * once all the purchased tickets has beeing included to his list, the user will be saved in its repository. 
      * @param type is the type of the ticket.
      * @param number is the ammount of tickets de user has purchased.
+     * @param concertId is the identification number for a concert.
      */
     public void associateUserWithTicket(String type, int number, long concertId) {
         List<Ticket> userTickets = userRepository.findById(this.activeUser.getId()).getTicketsList();
         Concert concert = this.concertRepository.findConcertById(concertId);
         for (int i = 0; i < number; i++) {
-            /* The user is null, because is an available ticket. */
-            this.createTicket(type, concert.getPrice(), concert);
-            Ticket newTicket = this.ticketRepository.findTicketByZoneAndTicketUser(type, null);
+            Ticket newTicket = this.createTicket(type, concert.getPrice(), concert); /* Creation of ticket. */
             userTickets.add(newTicket); /* Adding the ticket to the user ticket list. */
             newTicket.setUser(this.userRepository.findById(this.activeUser.getId())); /* Associating the ticket to the user. */
             this.ticketRepository.save(newTicket); /* Once it has been asssociated, the ticket is saved in its repository. */
