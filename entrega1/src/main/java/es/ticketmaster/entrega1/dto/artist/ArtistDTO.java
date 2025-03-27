fc05cfe5cd1d228fdc408c4137921a4768e29cb2
armin <armingarc@yahoo.com>
2025-03-27 10:09:37 +0100

    ArtistDTO and ShowUserDTO created

---
+++ entrega1/src/main/java/es/ticketmaster/entrega1/dto/artist/ArtistDTO.java
@@ -0,0 +1,7 @@
+package es.ticketmaster.entrega1.dto.artist;
+
+import java.time.LocalDateTime;
+
+public record ArtistDTO(Long id, String name, int popularityIndex, LocalDateTime sessionCreated, boolean hasPage, 
+    String mainInfo, String extendedInfo, String bestAlbumSpotifyLink, String bestAlbumAppleLink, String latestAlbumSpotifyLink, 
+    String latestAlbumAppleLink, String musicLink) {}
---
+++ entrega1/src/main/java/es/ticketmaster/entrega1/dto/user/ShowUserDTO.java
@@ -0,0 +1,3 @@
+package es.ticketmaster.entrega1.dto.user;
+
+public record ShowUserDTO(Long id, String username, String country, String email) {}
