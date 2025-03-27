0b11b2eb6f846ebb8f91919e7032e694fa2f1271
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-03-27 11:05:11 +0100

    Added the UserDTO methods to its Mapper

--- entrega1/src/main/java/es/ticketmaster/entrega1/dto/user/UserMapper.java
+++ entrega1/src/main/java/es/ticketmaster/entrega1/dto/user/UserMapper.java
@@ -9,6 +9,11 @@ import es.ticketmaster.entrega1.model.UserEntity;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    UserDTO toPrincipalDTO(UserEntity user);
+    List<UserDTO> toPrincipalDTOs(Collection<UserEntity> users);
+    UserEntity toDomain(UserDTO userDTO);
+
+
     ShowUserDTO toShowUserDTO(UserEntity user);
     List<ShowUserDTO> toShowUserDTOs(Collection<UserEntity> users);
     UserEntity toShowUserDTO(ShowUserDTO userDTO);
