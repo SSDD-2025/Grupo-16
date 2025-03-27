6edc5080617286bd7cc088c834266fa2970565ef
davidrisqgom <diznerr@gmail.com>
2025-03-27 10:35:37 +0100

    Completed ConcertMapper

--- entrega1/src/main/java/es/ticketmaster/entrega1/dto/concert/ConcertMapper.java
+++ entrega1/src/main/java/es/ticketmaster/entrega1/dto/concert/ConcertMapper.java
@@ -1,5 +1,8 @@
 package es.ticketmaster.entrega1.dto.concert;
 
+import java.util.Collection;
+import java.util.List;
+import org.mapstruct.Mapper;
 import es.ticketmaster.entrega1.model.Concert;
 
 @Mapper(componentModel = "spring")
@@ -7,4 +10,6 @@ public interface ConcertMapper {
     ConcertDTO toDTO(Concert concert);
 
     Concert toDomain(ConcertDTO concertDTO);
+
+    List<ConcertDTO> toDTOs(Collection<Concert> concert);
 }
