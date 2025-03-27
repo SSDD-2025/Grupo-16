6d85c02e95d046ccb1cd1255ecde9ed3dba7927a
armin <armingarc@yahoo.com>
2025-03-27 10:41:14 +0100

    Fixed errors (parcial)

--- entrega1/src/main/java/es/ticketmaster/entrega1/dto/user/ShowUserDTO.java
+++ entrega1/src/main/java/es/ticketmaster/entrega1/dto/user/ShowUserDTO.java
@@ -1,3 +1,4 @@
 package es.ticketmaster.entrega1.dto.user;
 
+/* It is also used to modify user */
 public record ShowUserDTO(Long id, String username, String country, String email) {}
--- entrega1/src/main/java/es/ticketmaster/entrega1/dto/user/UserMapper.java
+++ entrega1/src/main/java/es/ticketmaster/entrega1/dto/user/UserMapper.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import es.ticketmaster.entrega1.model.UserEntity;
 
@@ -11,5 +12,11 @@ import es.ticketmaster.entrega1.model.UserEntity;
 public interface UserMapper {
     ShowUserDTO toShowUserDTO(UserEntity user);
     List<ShowUserDTO> toShowUserDTOs(Collection<UserEntity> users);
+
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "roles", ignore = true)
+    @Mapping(target = "artistsList", ignore = true)
+    @Mapping(target = "ticketList", ignore = true)
+    @Mapping(target = "profilePicture", ignore = true)
     UserEntity toShowUserDTO(ShowUserDTO userDTO);
 }
