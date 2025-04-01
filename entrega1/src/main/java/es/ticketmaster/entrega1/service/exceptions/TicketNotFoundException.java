2bccadadde37603f9a7c49eb2dcadef3156e75ab
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-04-01 19:29:58 +0200

    Fix documentation

--- entrega1/src/main/java/es/ticketmaster/entrega1/service/exceptions/TicketNotFoundException.java
+++ entrega1/src/main/java/es/ticketmaster/entrega1/service/exceptions/TicketNotFoundException.java
@@ -1,7 +1,7 @@
 package es.ticketmaster.entrega1.service.exceptions;
 
 /**
- * Exception thrown when a user with the given ID is not found.
+ * Exception thrown when a ticket with the given ID is not found.
  */
 public class TicketNotFoundException extends RuntimeException {
     
