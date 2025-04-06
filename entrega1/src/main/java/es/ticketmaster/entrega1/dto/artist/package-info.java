cc8d47eea64034fd2aeec9c6f00940c669ebb8c5
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-04-06 20:15:21 +0200

    Add package documentation

---
+++ entrega1/src/main/java/es/ticketmaster/entrega1/dto/artist/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package contains the Data Transfer Object (DTO) for the Artist entity,
+ * along with its corresponding Mapper.
+ *
+ * The DTO is used to transfer artist-related data between different layers of the application,
+ * helping to decouple the internal domain model from external representations.
+ *
+ * The Mapper is responsible for converting between the domain model and the DTO,
+ * ensuring a clean and consistent transformation in both directions.
+ */
+package es.ticketmaster.entrega1.dto.artist;
