8aea71c388a65b32e1c5103fa9fa209c1c95bd3d
Michel Maes <michel.maes95@gmail.com>
2025-04-14 17:00:55 +0200

    Fix api-docs

--- entrega1/src/main/java/es/ticketmaster/entrega1/security/WebSecurityConfig.java
+++ entrega1/src/main/java/es/ticketmaster/entrega1/security/WebSecurityConfig.java
@@ -24,21 +24,21 @@ import es.ticketmaster.entrega1.security.jwt.UnauthorizedHandlerJwt;
 public class WebSecurityConfig {
 
     @Autowired
-	private JwtRequestFilter jwtRequestFilter;
+    private JwtRequestFilter jwtRequestFilter;
 
     @Autowired
     private UnauthorizedHandlerJwt unauthorizedHandlerJwt;
 
     @Autowired
     public SecurityUserDetails securityUserDetails;
-    
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
+    public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(securityUserDetails);
         authProvider.setPasswordEncoder(passwordEncoder());
@@ -46,50 +46,50 @@ public class WebSecurityConfig {
     }
 
     @Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-		return authConfig.getAuthenticationManager();
-	}
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 
     @Bean
     @Order(1)
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http.authenticationProvider(authenticationProvider());
         http
-            .securityMatcher("/api/**")
-            .exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+                .securityMatcher("/api/**")
+                .exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
 
         http
-            .authorizeHttpRequests(authorize -> authorize
-                    // AUTHENTICATION ENDPOINTS
-                    .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
-                    
-                    // PRIVATE ENDPOINTS:
-                    // UserRestController:
-                    .requestMatchers(HttpMethod.GET, "/api/users/**").hasAnyRole("USER", "ADMIN")
-                    .requestMatchers(HttpMethod.POST, "/api/users/**").permitAll()
-                    .requestMatchers(HttpMethod.PUT, "/api/users/**").hasAnyRole("USER", "ADMIN")
-                    .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasAnyRole("USER", "ADMIN")
+                .authorizeHttpRequests(authorize -> authorize
+                        // AUTHENTICATION ENDPOINTS
+                        .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
 
-                    // ConcertRestController
-                    .requestMatchers(HttpMethod.GET, "/api/concerts/**").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/api/concerts/**").hasRole("ADMIN")
-                    .requestMatchers(HttpMethod.PUT, "/api/concerts/**").hasRole("ADMIN")
-                    .requestMatchers(HttpMethod.DELETE, "/api/concerts/**").hasRole("ADMIN")
+                        // PRIVATE ENDPOINTS:
+                        // UserRestController:
+                        .requestMatchers(HttpMethod.GET, "/api/users/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/users/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/api/users/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasAnyRole("USER", "ADMIN")
 
-                    // ArtistRestController
-                    .requestMatchers(HttpMethod.GET, "/api/artists/**").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/api/artists/**").hasRole("ADMIN")
-                    .requestMatchers(HttpMethod.PUT, "/api/artists/**").hasRole("ADMIN")
-                    .requestMatchers(HttpMethod.DELETE, "/api/artists/**").hasRole("ADMIN")
+                        // ConcertRestController
+                        .requestMatchers(HttpMethod.GET, "/api/concerts/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/concerts/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/concerts/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/concerts/**").hasRole("ADMIN")
 
-                    // TicketRestController
-                    .requestMatchers(HttpMethod.GET, "/api/tickets/**").hasAnyRole("USER", "ADMIN")
-                    .requestMatchers(HttpMethod.POST, "/api/concert/**").hasRole("ADMIN")
-                    .requestMatchers(HttpMethod.PUT, "/api/tickets/**").hasRole("ADMIN")
-                    .requestMatchers(HttpMethod.DELETE, "/api/tickets/**").hasRole("ADMIN")
+                        // ArtistRestController
+                        .requestMatchers(HttpMethod.GET, "/api/artists/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/artists/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/artists/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/artists/**").hasRole("ADMIN")
 
-                    // PUBLIC ENDPOINTS:
-                    .anyRequest().permitAll());
+                        // TicketRestController
+                        .requestMatchers(HttpMethod.GET, "/api/tickets/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/concert/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/tickets/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/tickets/**").hasRole("ADMIN")
+
+                        // PUBLIC ENDPOINTS:
+                        .anyRequest().permitAll());
 
         // Disable Form login Authentication
         http.formLogin(formLogin -> formLogin.disable());
@@ -108,7 +108,7 @@ public class WebSecurityConfig {
 
         return http.build();
     }
-    
+
     @Bean
     @Order(2)
     public SecurityFilterChain webfilterChain(HttpSecurity http) throws Exception {
@@ -116,42 +116,43 @@ public class WebSecurityConfig {
         http.authenticationProvider(authenticationProvider());
 
         http
-            .authorizeHttpRequests(authorize -> authorize
-                // Access to static files
-                .requestMatchers("/css/**").permitAll()
-                .requestMatchers("/js/**").permitAll()
-                .requestMatchers("/DDBB/**").permitAll()
-                .requestMatchers("/media/**").permitAll()
-                .requestMatchers("/concert/*/download-poster").permitAll()
-                .requestMatchers("/artist/*/download-photo").permitAll()
+                .authorizeHttpRequests(authorize -> authorize
+                        // Access to static files
+                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers("/js/**").permitAll()
+                        .requestMatchers("/DDBB/**").permitAll()
+                        .requestMatchers("/media/**").permitAll()
+                        .requestMatchers("/concert/*/download-poster").permitAll()
+                        .requestMatchers("/artist/*/download-photo").permitAll()
 
-                // Public Pages
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/login").permitAll()
-                .requestMatchers("/sign-up/**").permitAll()
-                .requestMatchers("/artist/**").permitAll()
-                .requestMatchers("/sign-in").permitAll()
-                // Error Page
-                .requestMatchers("/error").permitAll()
-                // Registered User Pages
-                .requestMatchers("/profile/**").hasAnyRole("USER", "ADMIN")
-                .requestMatchers("/concert/**").hasAnyRole("USER", "ADMIN")
-                .requestMatchers("/ticket/**").hasAnyRole("USER", "ADMIN")
-                // Administrator Pages
-                .requestMatchers("/admin/**").hasAnyRole("ADMIN")
-            )
-            .formLogin(formLogin -> formLogin
-                .loginPage("/sign-in")
-                .loginProcessingUrl("/login")
-                .failureUrl("/sign-in?error=true")
-                .defaultSuccessUrl("/")
-                .permitAll()
-            )
-            .logout(logout -> logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/")
-                .permitAll()
-            );
+                        .requestMatchers("/v3/api-docs*/**").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+
+                        // Public Pages
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/sign-up/**").permitAll()
+                        .requestMatchers("/artist/**").permitAll()
+                        .requestMatchers("/sign-in").permitAll()
+                        // Error Page
+                        .requestMatchers("/error").permitAll()
+                        // Registered User Pages
+                        .requestMatchers("/profile/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/concert/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/ticket/**").hasAnyRole("USER", "ADMIN")
+                        // Administrator Pages
+                        .requestMatchers("/admin/**").hasAnyRole("ADMIN"))
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/sign-in")
+                        .loginProcessingUrl("/login")
+                        .failureUrl("/sign-in?error=true")
+                        .defaultSuccessUrl("/")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .permitAll());
 
         return http.build();
     }
