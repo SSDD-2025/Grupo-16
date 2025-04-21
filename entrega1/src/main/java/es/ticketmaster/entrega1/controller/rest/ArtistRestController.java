25dbd873f1509292bffdeebb5bfd82a8035a230a
davidrisqgom <diznerr@gmail.com>
2025-04-22 00:53:16 +0200

    ArtistRestController annotation documentation.

--- entrega1/src/main/java/es/ticketmaster/entrega1/controller/rest/ArtistRestController.java
+++ entrega1/src/main/java/es/ticketmaster/entrega1/controller/rest/ArtistRestController.java
@@ -25,10 +25,13 @@ import static org.springframework.web.servlet.support.ServletUriComponentsBuilde
 
 import es.ticketmaster.entrega1.dto.artist.ArtistDTO;
 import es.ticketmaster.entrega1.service.ArtistService;
-
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@Tag(name = "Artists", description = "Endpoints for managing artist resources")
 @RequestMapping("/api/artists")
 public class ArtistRestController {
 
@@ -36,16 +39,12 @@ public class ArtistRestController {
     private ArtistService artistService;
 
     /*ARTIST SEARCH CONTROLLER METHODS*/
-
-    /**
-     * REST Controller that returns all the artists in the DDBB
-     * 
-     * @return A Collection of all the artist's DTOs.
-     */
+    @Operation(summary = "Get all the existing artist.", description = "Returns a page of artists. The page size is modificable, but its default size is 0. As well, the default page is the first one.")
+    @ApiResponse(responseCode = "200", description = "All artists fetched successfully.")
     @GetMapping
-    public Page<ArtistDTO> getAllArtists(@PageableDefault(page = 0, size = 10) Pageable pageable, @RequestParam(name= "search", required = false) String search){
+    public Page<ArtistDTO> getAllArtists(@PageableDefault(page = 0, size = 10) Pageable pageable, @RequestParam(name = "search", required = false) String search) {
 
-        if (search == null){
+        if (search == null) {
             return artistService.getEveryArtist(pageable);
         } else {
             return artistService.getSearchDTOBy(search, pageable);
@@ -53,26 +52,22 @@ public class ArtistRestController {
 
     }
 
-    /**
-     * REST Controller that returns the DTO of an artist by its ID
-     * 
-     * @param id ID of the artist to search.
-     * @return DTO of the artist to search.
-     */
+    @Operation(summary = "Get an specific artist.", description = "Returns the path-specified by id Artist information. If any error occurs, a personalized error DTO will be returned with its specific information.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Artist obtained successfully."),
+        @ApiResponse(responseCode = "404", description = "Not Found: Artist not found."),})
     @GetMapping("/{id}")
-    public ArtistDTO getArtist(@PathVariable long id){
+    public ArtistDTO getArtist(@PathVariable long id) {
 
         return artistService.getArtist(id);
 
     }
 
-    /**
-     * REST Controller that handles the display of the Artist's main photo
-     * 
-     * @param id the ID of the artist which main photo is searched
-     * @return ResponseEntity with the image if everything went correctly, the error JSON in other case
-     * @throws IOException
-     */
+    @Operation(summary = "Get the photo of an Artist.", description = "Returns the path-specified by id Artist image. If any error occurs, a personalized error DTO will be returned with its specific information.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Photo obtained successfully."),
+        @ApiResponse(responseCode = "404", description = "Not Found: Artist or photo not found.")
+    })
     @GetMapping("/{id}/photo")
     public ResponseEntity<Object> getPhoto(@PathVariable long id) throws SQLException, IOException {
 
@@ -83,17 +78,17 @@ public class ArtistRestController {
 
 
     /*ARTIST CREATION CONTROLLER METHODS*/
+    @Operation(summary = "Create a new Artist", description = "Creates an the post-specified Artist and returns its saved information. If any error occurs, a personalized error DTO will be returned with its specific information.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Artist created with success."),
+        @ApiResponse(responseCode = "401", description = "Unauthorized: User is not authenticated."),
+        @ApiResponse(responseCode = "405", description = "Bad Request: User is not allowed to execute this function."),
+        @ApiResponse(responseCode = "400", description = "Bad Request: Invalid data or format. The Artist's name is repeated."),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error: Unexpected error.")
+    })
+    @PostMapping
+    public ResponseEntity<ArtistDTO> createArtist(@RequestBody ArtistDTO artistDTO) throws Exception {
 
-    /**
-     * REST Controller that handles the creation of an artist
-     * 
-     * @param artistDTO DTO sent by the request with the attributes.
-     * @return DTO of the artist after it is introduced in the DDBB.
-     * @throws Exception
-     */
-    @PostMapping 
-    public ResponseEntity<ArtistDTO> createArtist(@RequestBody ArtistDTO artistDTO) throws Exception{
-        
         artistDTO = artistService.registerNewArtist(artistDTO);
 
         URI location = fromCurrentRequest().path("/id").buildAndExpand(artistDTO.id()).toUri();
@@ -101,14 +96,15 @@ public class ArtistRestController {
         return ResponseEntity.created(location).body(artistDTO);
     }
 
-    /**
-     * REST Controller that handles the creation of the Artist's main-photo
-     * 
-     * @param id the ID of the artist which main photo is going to be created
-     * @param imageFile the aledgly main-photo of the artist
-     * @return ResponseEntity with the state of the query
-     * @throws IOException
-     */
+    @Operation(summary = "Upload an Artist's photo", description = "Saves and activates the image as the path-specified by id Artist's photo. If any error occurs, a personalized error DTO will be returned with its specific information.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Image uploaded with success."),
+        @ApiResponse(responseCode = "400", description = "Bad Resquest: Invalid image format"),
+        @ApiResponse(responseCode = "404", description = "Not Found: Artist or photo not found."),
+        @ApiResponse(responseCode = "401", description = "Unauthorized: User is not authenticated."),
+        @ApiResponse(responseCode = "405", description = "Bad Request: User is not allowed to execute this function."),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error: Unexpected error.")
+    })
     @PostMapping("/{id}/photo")
     public ResponseEntity<Object> createMainPhoto(@PathVariable long id, @RequestParam MultipartFile imageFile) throws IOException {
 
@@ -121,15 +117,15 @@ public class ArtistRestController {
 
 
     /*ARTIST MODIFICATION CONTROLLER METHODS*/
-    
-    /**
-     * REST Controller that handles the modification of an artist.
-     * 
-     * @param id ID of the artist to modify.
-     * @param artistDTO DTO sent by the request with the modifications.
-     * @return DTO of the artist after it is modified in the DDBB.
-     * @throws IOException
-     */
+    @Operation(summary = "Modify an Artist", description = "Modifies the path-specified by id Artist and returns its saved information. If any error occurs, a personalized error DTO will be returned with its specific information.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Artist modified with success."),
+        @ApiResponse(responseCode = "400", description = "Bad Resquest: Invalid data or format."),
+        @ApiResponse(responseCode = "404", description = "Not Found: Artist not found."),
+        @ApiResponse(responseCode = "401", description = "Unauthorized: User is not authenticated."),
+        @ApiResponse(responseCode = "405", description = "Bad Request: User is not allowed to execute this function."),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error: Unexpected error.")
+    })
     @PutMapping("/{id}")
     public ArtistDTO modifyArtist(@PathVariable long id, @RequestBody ArtistDTO artistDTO) throws IOException {
 
@@ -137,14 +133,15 @@ public class ArtistRestController {
 
     }
 
-    /**
-     * REST Controller that handles the modification of the Artist's main-photo
-     * 
-     * @param id the ID of the artist which main photo is going to be modified
-     * @param imageFile the new desired main-photo of the artist
-     * @return ResponseEntity with the state of the query
-     * @throws IOException
-     */
+    @Operation(summary = "Modifies an Artist's photo", description = "Saves and activates the image as the path-specified by id Artist's photo. If any error occurs, a personalized error DTO will be returned with its specific information.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Image uploaded with success."),
+        @ApiResponse(responseCode = "400", description = "Bad Resquest: Invalid image format"),
+        @ApiResponse(responseCode = "404", description = "Not Found: Artist or photo not found."),
+        @ApiResponse(responseCode = "401", description = "Unauthorized: User is not authenticated."),
+        @ApiResponse(responseCode = "405", description = "Bad Request: User is not allowed to execute this function."),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error: Unexpected error.")
+    })
     @PutMapping("/{id}/photo")
     public ResponseEntity<Object> replacePhoto(@PathVariable long id, @RequestParam MultipartFile imageFile) throws IOException {
 
@@ -155,26 +152,30 @@ public class ArtistRestController {
 
 
     /*ARTIST DELETION CONTROLLER METHODS.*/
-
-    /**
-     * REST Controller that handles the deletion of an artist.
-     * @param id ID of the artist to delete.
-     * @return DTO of the artist as it previously was in the DDBB.
-     */
+    @Operation(summary = "Delete an Artist", description = "Deletes the path-specified by id Artist and returns its previous-to-deletion information. If any error occurs, a personalized error DTO will be returned with its specific information.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Artist deleted with success."),
+        @ApiResponse(responseCode = "400", description = "Bad Resquest: Invalid data or format."),
+        @ApiResponse(responseCode = "404", description = "Not Found: Artist not found."),
+        @ApiResponse(responseCode = "401", description = "Unauthorized: User is not authenticated."),
+        @ApiResponse(responseCode = "405", description = "Bad Request: User is not allowed to execute this function."),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error: Unexpected error.")
+    })
     @DeleteMapping("/{id}")
-    public ArtistDTO deleteArtist(@PathVariable long id){
+    public ArtistDTO deleteArtist(@PathVariable long id) {
 
         return artistService.deleteArtistWithId(id);
 
     }
 
-    /**
-     * REST Controller that handles the deletion of the Artist's main photo
-     * 
-     * @param id the ID of the artist which main photo is going to be deleted
-     * @return ResponseEntity with the JSON to show how the transaction went
-     * @throws IOException
-     */
+    @Operation(summary = "Deletes an Artist's photo", description = "Deletes the photo of the path-specified by id Artist. If any error occurs, a personalized error DTO will be returned with its specific information.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Image deleted with success."),
+        @ApiResponse(responseCode = "404", description = "Not Found: Artist or photo not found."),
+        @ApiResponse(responseCode = "401", description = "Unauthorized: User is not authenticated."),
+        @ApiResponse(responseCode = "405", description = "Bad Request: User is not allowed to execute this function."),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error: Unexpected error.")
+    })
     @DeleteMapping("/{id}/photo")
     public ResponseEntity<Object> deleteMainPhoto(@PathVariable long id) throws SQLException, IOException {
 
