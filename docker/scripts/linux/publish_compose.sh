95d47e04edb415ddd8fe5bd0c49c7f96115602d0
davidrisqgom <diznerr@gmail.com>
2025-05-13 13:47:00 +0200

    Created docker-compose.local.yml

--- docker/scripts/linux/publish_compose.sh
+++ docker/scripts/linux/publish_compose.sh
@@ -1,1 +1,24 @@
-# Script de bash que publicará el fichero docker-compose.prod.yml en DockerHub como un OCI Artifact.
+#!/bin/bash
+# Abort script execution on error
+set -e
+
+# Variables
+USERNAME="liveticket"
+TAG_VERSION="1.0.0"
+COMPOSE_FILE="docker-compose.local.yml" # This script should be executed in the directory where the COMPOSE_FILE is
+APP_NAME="liveticket"
+
+#Publication format
+PUBLICATION=$USERNAME/$APP_NAME-compose:$TAG_VERSION
+
+# Login the user
+docker login
+
+# Inform the user of the process
+echo "⚙️ Publishing the Docker Compose file"
+
+# Publish the Docker Compose
+docker compose -f $COMPOSE_FILE publish $PUBLICATION
+
+# Notify via bash
+echo "✅ Compose published correctly in docker.io/$PUBLICATION"
